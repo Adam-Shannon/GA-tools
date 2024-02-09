0ba2313a4a3e5561304215af9501927dbb4fe907
Adam <adam.shannon02@gmail.com>
2024-02-09 20:15:48 +0000

    Shell CLI project for rust GA-tools

--- ga-rust/src/main.rs
+++ ga-rust/src/main.rs
@@ -1,17 +1,30 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
-    mode: String,
+    pattern: String,
     /// The path to the file to read
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    println!("mode: {:?}, path: {:?}", args.mode, args.path)
-}
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
+    let pattern = &args.pattern;
+
+    // let progress = indicatif::ProgressBar::new(100);
+    // for i in 0..100{
+    //     // progress.println(format!("[+] finished #{}", i));
+    //     progress.inc(1);
+    // }    
+
+    genes::find_matches(&content, pattern, &mut std::io::stdout());
+
+    Ok(())
+}
